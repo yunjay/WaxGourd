082775f4814cfe1c4512d395a23967724cda5c60
Yun Jay Kim <yunjaysus@gmail.com>
2025-02-27 16:23:52 +0900

    Fence class

---
+++ RenderEngine/Fence.cpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "Fence.h"
+#include "Device.h"
+#include "CommandQueue.h"
+
+namespace WaxGourd {
+Fence::Fence(Device* _device, std::string _name) {
+	ID3D12Device* device = (ID3D12Device*)_device->GetDevice();
+
+	ASSERT_HRESULT(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(m_fence.GetAddressOf())));
+	m_fence->SetName(_name.c_str());
+	
+	// Event for this fence
+	m_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+	return true;
+}
+
+void Fence::Wait(FenceValue value) {
+	if (!IsCompleted(value)) {
+		m_fence->SetEventOnCompletion(value, m_event);
+		WaitForSingleObjectEx(m_event, INFINITE);
+	}
+}
+
+void Fence::Signal(FenceValue value) {
+	m_fence->Signal(value);
+}
+
+bool Fence::IsCompleted(FenceValue value) {
+	// Fence values are incremental
+	return this->GetCompletedValue() >= value;
+}
+
+FenceValue Fence::GetCompletedValue() const {
+	return m_fence->GetCompletedValue();
+}
+
+}
+
---
+++ RenderEngine/Fence.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <d3d12.h>
+#include <string>
+#include "CommandQueue.h"
+#include "ComPointer.h"
+#include "Device.h"
+#include "MathDefines.h"
+
+namespace WaxGourd
+{
+typedef uint64 FenceValue;
+class Fence
+{
+public:
+	Fence(Device* _device, std::string _name);
+	~Fence() = default;
+
+	void Wait(FenceValue _value);
+	void Signal(FenceValue _value);
+
+	bool IsCompleted(FenceValue _value);
+	FenceValue GetCompletedValue() const;
+
+	operator ID3D12Fence* () const { return m_fence.Get(); }
+	ID3D12Fence* GetFence() const { return m_fence.Get(); }
+private:
+	ComPointer<ID3D12Fence> m_fence = nullptr;
+	HANDLE m_event = nullptr;
+};
+}
