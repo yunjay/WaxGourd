39d3d65c6b490df4355ff80392e483085941963b
Yun Jay Kim <yunjaysus@gmail.com>
2025-03-11 21:05:05 +0900

    Create Macros.h

---
+++ Utility/Macros.h
@@ -0,0 +1,10 @@
+
+#define ASSERT_HRESULT(hr) if(FAILED(hr)) assert(false);
+
+#define DELETE_COPY_OPERATORS(Class)\
+        Class(Class const&)            = delete;\
+        Class& operator=(Class const&) = delete;
+
+#define DELETE_MOVE_OPERATORS(Class)\
+        Class(Class&&) noexcept            = delete; \
+        Class& operator=(Class&&) noexcept = delete;
