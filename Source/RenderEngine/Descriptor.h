64eec3e87e36aac45fa3ad8cd916649f2c874ee6
Yun Jay Kim <yunjaysus@gmail.com>
2025-08-13 21:59:17 +0900

    Descriptor

--- Source/RenderEngine/Descriptor.h
+++ Source/RenderEngine/Descriptor.h
@@ -3,16 +3,19 @@
 #include <directx/d3d12.h>
 #define D3D12_GPU_VIRTUAL_ADDRESS_INVALID ((D3D12_GPU_VIRTUAL_ADDRESS)-1)
 namespace WaxGourd {
-
-class Descriptor
-{
+class Descriptor {
+protected:
+	friend class DescriptorHeap;
+    D3D12_CPU_DESCRIPTOR_HANDLE m_cpu_handle;
+    D3D12_GPU_DESCRIPTOR_HANDLE m_gpu_handle;
 public:
+    uint32 m_index = uint32_MAX;
     Descriptor() {
         m_cpu_handle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_INVALID;
         m_gpu_handle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_INVALID;
     }
     Descriptor(D3D12_CPU_DESCRIPTOR_HANDLE cpu_handle)
-        : m_cpu_handle(cpu_handle){
+        : m_cpu_handle(cpu_handle) {
         m_gpu_handle.ptr = D3D12_GPU_VIRTUAL_ADDRESS_INVALID;
     }
     // Shader visible
@@ -25,17 +28,20 @@ public:
     D3D12_GPU_DESCRIPTOR_HANDLE GetGpuAddress() const { return m_gpu_handle; }
     size_t GetCpuHandlePtr() const { return m_cpu_handle.ptr; }
     uint64_t GetGpuHandlePtr() const { return m_gpu_handle.ptr; }
+    uint32 GetIndex() const { return m_index; }
     bool IsNull() const { return m_cpu_handle.ptr == D3D12_GPU_VIRTUAL_ADDRESS_INVALID; }
+    bool IsValid() const { return !IsNull(); }
     bool IsShaderVisible() const { return m_gpu_handle.ptr != D3D12_GPU_VIRTUAL_ADDRESS_INVALID; }
     // Increment
-    void operator += (int offsetTimesDescriptorSize){
-        if (m_cpu_handle.ptr != D3D12_GPU_VIRTUAL_ADDRESS_INVALID)
-            m_cpu_handle.ptr += offsetTimesDescriptorSize;
-        if (m_gpu_handle.ptr != D3D12_GPU_VIRTUAL_ADDRESS_INVALID)
-            m_gpu_handle.ptr += offsetTimesDescriptorSize;
+    void Increment(uint32 increment, uint32 multiply = 1) {
+        m_cpu_handle.ptr += increment * multiply;
+        if (m_gpu_handle.ptr != D3D12_GPU_VIRTUAL_ADDRESS_INVALID) 
+            m_gpu_handle.ptr += increment * multiply;
+        m_index += multiply;
+    }		
+    bool operator==(Descriptor const& other) const {
+        return (m_cpu_handle.ptr == other.m_cpu_handle.ptr) 
+            && (m_index == other.m_index);
     }
-private:
-    D3D12_CPU_DESCRIPTOR_HANDLE m_cpu_handle;
-    D3D12_GPU_DESCRIPTOR_HANDLE m_gpu_handle;
 };
 }
