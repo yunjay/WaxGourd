8d74e08282d19327e28a7ef17193a32327eba63e
Yun Jay Kim <yunjaysus@gmail.com>
2025-10-28 15:20:48 +0900

    StringUtility

--- Source/Utility/StringUtility.cpp
+++ Source/Utility/StringUtility.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <codecvt>
 #include <locale>
+#include <filesystem>
 namespace WaxGourd {
 std::wstring ToWideString(std::string const& str) {
 	int num_chars = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.length(), NULL, 0);
@@ -27,5 +28,38 @@ std::string ToString(float3 const& vec) {
 }
 std::string ToString(float4 const& vec) {
 	return "(" + std::to_string(vec.x) + "," + std::to_string(vec.y) + "," + std::to_string(vec.z) + "," + std::to_string(vec.w) + ")";
+}	
+std::string GetParentPath(std::string_view complete_path) {
+	std::filesystem::path p(complete_path);
+	return p.parent_path().string();
+}
+std::string GetFilename(std::string_view complete_path) {
+	std::filesystem::path p(complete_path);
+	return p.filename().string();
+}
+std::string GetFilenameWithoutExtension(std::string_view complete_path) {
+	std::filesystem::path p(complete_path);
+	return p.filename().replace_extension().string();
+}
+bool FileExists(std::string_view file_path) {
+	std::filesystem::path p(file_path);
+	return std::filesystem::exists(p);
+}
+std::string GetExtension(std::string_view path) {
+	std::filesystem::path p(path);
+	return p.extension().string();
+}
+std::string MakeAllLowercase(std::string const& input) {
+	// ASCII-only case conversion; for full Unicode need some library support.
+	auto view = input | std::views::transform([](unsigned char ch) {
+		return static_cast<char>(std::tolower(ch));
+		});
+	return std::ranges::to<std::string>(view);
+}
+std::string MakeAllUppercase(std::string const& input) {
+	auto view = input | std::views::transform([](unsigned char ch) {
+		return static_cast<char>(std::toupper(ch));
+		});
+	return std::ranges::to<std::string>(view);
 }
 }
--- Source/Utility/StringUtility.h
+++ Source/Utility/StringUtility.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "MathDefines.h"
 #include <string>
+#include <string_view>
 namespace WaxGourd {
 	std::wstring ToWideString(std::string const&);
 	std::string ToString(std::wstring const&);
 	std::string ToString(float3 const&);
 	std::string ToString(float4 const&);
+
+	std::string GetParentPath(std::string_view complete_path);
+	std::string GetFilename(std::string_view complete_path);
+	std::string GetFilenameWithoutExtension(std::string_view complete_path);
+	bool FileExists(std::string_view file_path);
+	std::string GetExtension(std::string_view path);
+	std::string MakeAllLowercase(std::string const&);
+	std::string MakeAllUppercase(std::string const&);
 }
